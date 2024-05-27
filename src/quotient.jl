44b7c78c676903e48920ad745dead2d83935a7b9
Benoît Legat <benoit.legat@gmail.com>
2024-05-27 18:21:23 +0200

    Fixes

--- src/quotient.jl
+++ src/quotient.jl
@@ -10,3 +10,11 @@ Base.length(basis::QuotientBasis) = length(basis.basis)
 function MP.coefficients(p, basis::QuotientBasis)
     return MP.coefficients(rem(p, basis.divisor), basis.basis)
 end
+
+function SA.coeffs(p, ::FullBasis{Monomial}, basis::QuotientBasis)
+    return MP.coefficients(p, basis)
+end
+
+function SA.coeffs(coeffs, sub::SubBasis{Monomial}, basis::QuotientBasis)
+    return SA.coeffs(MP.polynomial(coeffs, sub.monomials), parent(sub), basis)
+end
