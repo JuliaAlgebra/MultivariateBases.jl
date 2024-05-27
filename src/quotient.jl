51e17cc3190ff45fc43daba292cbc3def9c5b9f2
Benoît Legat <benoit.legat@gmail.com>
2024-05-27 10:36:32 +0200

    Implement iterate for QuotientBasis

--- src/quotient.jl
+++ src/quotient.jl
@@ -3,6 +3,8 @@ struct QuotientBasis{T,I,B<:SA.AbstractBasis{T,I},D} <: SA.ExplicitBasis{T,I}
     divisor::D
 end
 
+Base.iterate(basis::QuotientBasis) = iterate(basis.basis)
+Base.iterate(basis::QuotientBasis, s) = iterate(basis.basis, s)
 Base.length(basis::QuotientBasis) = length(basis.basis)
 
 function MP.coefficients(p, basis::QuotientBasis)
