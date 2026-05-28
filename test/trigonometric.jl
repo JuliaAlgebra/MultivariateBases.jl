3ae02c6b486e523f42309aab68902d68f17b859f
Benoît Legat <benoit.legat@gmail.com>
2026-05-28 14:08:11 +0200

    Fix univariate_mul for trigonometric basis (#81)
    * Fix univariate_mul for trigonometric basis
    
    * Fix format

--- test/trigonometric.jl
+++ test/trigonometric.jl
@@ -17,4 +17,9 @@ using DynamicPolynomials
     )
     e = MB.algebra_element(MB.sparse_coefficients(1//1 * x^0), full)
     @test a * d == a * e
+    # sin(ωt) * cos(2ωt) = (sin(3ωt) - sin(ωt)) / 2
+    # Exponents: sin(ωt) -> x^2, sin(3ωt) -> x^6, cos(2ωt) -> x^3
+    s1 = MB.algebra_element(MB.sparse_coefficients(1//1 * x^2), full)
+    c2 = MB.algebra_element(MB.sparse_coefficients(1//1 * x^3), full)
+    @test (s1 * c2).coeffs == MB.sparse_coefficients(-1//2 * x^2 + 1//2 * x^6)
 end
